ac0b700bf300cdb5df69cad92e9dc0ee2c3f3aff
Atin Gupta <atingupta2005@gmail.com>
2026-02-16 18:56:26 +0530

    -

---
+++ commands.sh
@@ -0,0 +1,25 @@
+256  cat .gitignore 
+  257  git init 
+  258  git branch -m master
+  259  git add *
+  260  git commit -m "My first Commit"
+  261  git status
+  262  git add .gitignore 
+  263  git commit -m "My first Commit"
+  264  git status
+  265  ls -al
+  266  git log
+  267  nano dbt_project.yml 
+  268  git diff
+  269  git commit -m "changed version"
+  270  git add *
+  271  git commit -m "changed version"
+  272  git status
+  273  git diff
+  274  ls
+  275  rm dbt_project.yml 
+  276  ls
+  277  git status
+  278  git stash
+  279  ls
+  280  history
